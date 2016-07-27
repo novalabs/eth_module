21d98d34ddfd6b43cffa6a77c97a87e4d2ef82f0
Davide Rizzi <d.rizzi@novalabs.io>
2016-07-27 11:45:42 +0200

    ...

--- include/Module.hpp
+++ include/Module.hpp
@@ -1,3 +1,9 @@
+/* COPYRIGHT (c) 2016 Nova Labs SRL
+ *
+ * All rights reserved. All use of this software and documentation is
+ * subject to the License Agreement located in the file LICENSE.
+ */
+
 #pragma once
 
 #include <core/mw/CoreModule.hpp>
@@ -11,8 +17,10 @@ public:
 	static bool
 	initialize();
 
+// --- Ethernet PHY control ---------------------------------------------------
 	static void enablePHY();
 	static void disablePHY();
+// ----------------------------------------------------------------------------
 
 	Module();
 	virtual ~Module() {}
